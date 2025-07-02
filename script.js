18148a68aea2e0b3257a4e7e446b428688ba7636
RICARDO YAN SANTOS PAIVA <ricardopaiva6177@digitalcollege.internal>
2025-07-01 21:51:25 -0300

    incluindo estados e cidades

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 
 const consultarCep = () => {
+
     console.log('chamou a API')
     const cep = document.getElementById('cep').value
 
@@ -15,10 +16,51 @@ const consultarCep = () => {
         document.getElementById('rua').value = json.address
         document.getElementById('bairro').value = json.district
         document.getElementById('cidade').value = json.city
-        document.getElementById('estado').value = json.state
+        document.getElementById('uf').value = json.state
         
+        fetchMunicipios(json.state)
     
-    
+
     })
+}
+
+const fetchEstados = () => {
+    let uri = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome'
+    console.log(`URI: ${uri}`)
+
+    fetch(uri)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+        let options = '<option select>Selecione...</option>'
+
+        data.forEach(estado => {
+            options = options + `<option value="${estado.sigla}">${estado.nome} (${estado.sigla})</option>`     
+        })
+
+        document.getElementById('uf').innerHTML = options
+
+    });
 
 }
+
+fetchEstados()
+
+
+
+const fetchMunicipios = (uf) => {
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`)
+    .then(response => response.json())
+    .then(json => {
+        console.log(json)
+
+        let options = '<option selected>Escolha</option>'
+
+        json.forEach(cidade => {
+            options = options + `<option value="${cidade.nome}">${cidade.nome}</option>`
+        })
+        document.getElementById('cidade').innerHTML = options
+    })
+}
+fetchMunicipios()
